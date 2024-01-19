a00b199be6cd543abf48106462a9eda10a03db6d
Edin Herić <84852928+hera-beast@users.noreply.github.com>
2024-01-19 11:10:24 +0100

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import MultiSelect from "./components/multiselect/MultiSelect.vue";
+import { Item } from "./types/types";
+
+const selected = ref<Item[]>([]);
+</script>
+
+<template>
+	<MultiSelect v-model="selected" apiUrl="http://localhost:3000/api/items" placeholder="Placeholder" />
+</template>
+
+<style lang="scss" scoped></style>
