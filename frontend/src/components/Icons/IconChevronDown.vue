83dbfb0437303d5f3c8692b1726aa91f480d1643
Edin Herić <84852928+hera-beast@users.noreply.github.com>
2024-01-19 11:16:53 +0100

    Add files via upload

---
+++ frontend/src/components/Icons/IconChevronDown.vue
@@ -0,0 +1,16 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20">
+		<path
+			fill="currentColor"
+			fillRule="evenodd"
+			d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06"
+			clipRule="evenodd"
+		></path>
+	</svg>
+</template>
+
+<script lang="ts">
+export default {
+	name: "HeroiconsChevronDown20Solid",
+};
+</script>
